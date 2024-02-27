98cd02af5d25593e5397c82770dbdf6574755baa
Pawon25 <pavanbasavaraj25@gmail.com>
2024-02-27 22:35:07 +0530

    Added comments, Array tracing strats with zero index

--- Kadans.java
+++ Kadans.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Kadans {
     public static void main(String[] args) {
         
+        //!Getting scanner
         Scanner scanner= new Scanner(System.in);
 
         System.out.print("Enter the size of an integer array:\t");
@@ -12,20 +13,23 @@ public class Kadans {
         for (int i = 0; i < arraysize; i++) {
             array[i]=scanner.nextInt();
         }
+
+        //!Setting up variables: We need max_so_far to get the max of subarray
         int max_so_far=Integer.MIN_VALUE;
         int max_ending_here=0;
 
-        for(int i=1;i<arraysize;i++){
+        for(int i=0;i<arraysize;i++){
 
             max_ending_here=max_ending_here+array[i];
             if (max_so_far<max_ending_here) {
                 max_so_far=max_ending_here;
+                System.out.println("Tracing from "+i);
             }
             if (max_ending_here<0) {
                 max_ending_here=0;
             }
-            System.out.println("Maximum Contigious array so far is "+max_so_far);
         }
+        System.out.println("Maximum Contigious array so far is "+max_so_far);
 
     }
     
