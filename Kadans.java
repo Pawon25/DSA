8d0e142bf77cceb7734f849cd54e2841df23342c
Pavan B <pavanbasavaraj25@gmail.com>
2024-02-28 20:29:00 +0530

    Update Kadans.java

--- Kadans.java
+++ Kadans.java
@@ -21,11 +21,11 @@ public class Kadans {
         for(int i=0;i<arraysize;i++){
 
             max_ending_here=max_ending_here+array[i];
-            if (max_so_far<max_ending_here) {
+            if (max_so_far < max_ending_here) {
                 max_so_far=max_ending_here;
                 System.out.println("Tracing from "+i);
             }
-            if (max_ending_here<0) {
+            if (max_ending_here < 0) {
                 max_ending_here=0;
             }
         }
