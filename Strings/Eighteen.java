f1dcdbf981c2ad63247320adb8364b05c196a74d
Pawon25 <pavanbasavaraj25@gmail.com>
2025-07-22 23:03:45 +0530

    Strings 22-07-2025

---
+++ Strings/Eighteen.java
@@ -0,0 +1,37 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+
+public class Eighteen{
+    public static boolean isPalindrome(String s){
+        s = s.replaceAll("[^a-zA-Z0-9]","");
+        s =s.toLowerCase();
+        int left = 0;
+        int right = s.length()-1;
+        while(left <= right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }left++; right--;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+        String s1 = "Hello World";
+        System.out.println(isPalindrome(s1));
+    }
+}
+
